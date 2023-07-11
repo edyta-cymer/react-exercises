161fae4dcabfd094c6a03af89e289450f364e049
Louisetrovato <73744715+Louisetrovato@users.noreply.github.com>
2023-07-11 12:40:18 +0200

    11-07

---
+++ 02-JS-review/script.js
@@ -0,0 +1,232 @@
+const data = [
+  {
+    id: 1,
+    title: "The Lord of the Rings",
+    publicationDate: "1954-07-29",
+    author: "J. R. R. Tolkien",
+    genres: [
+      "fantasy",
+      "high-fantasy",
+      "adventure",
+      "fiction",
+      "novels",
+      "literature",
+    ],
+    hasMovieAdaptation: true,
+    pages: 1216,
+    translations: {
+      spanish: "El señor de los anillos",
+      chinese: "魔戒",
+      french: "Le Seigneur des anneaux",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.52,
+        ratingsCount: 630994,
+        reviewsCount: 13417,
+      },
+      librarything: {
+        rating: 4.53,
+        ratingsCount: 47166,
+        reviewsCount: 452,
+      },
+    },
+  },
+  {
+    id: 2,
+    title: "The Cyberiad",
+    publicationDate: "1965-01-01",
+    author: "Stanislaw Lem",
+    genres: [
+      "science fiction",
+      "humor",
+      "speculative fiction",
+      "short stories",
+      "fantasy",
+    ],
+    hasMovieAdaptation: false,
+    pages: 295,
+    translations: {},
+    reviews: {
+      goodreads: {
+        rating: 4.16,
+        ratingsCount: 11663,
+        reviewsCount: 812,
+      },
+      librarything: {
+        rating: 4.13,
+        ratingsCount: 2434,
+        reviewsCount: 0,
+      },
+    },
+  },
+  {
+    id: 3,
+    title: "Dune",
+    publicationDate: "1965-01-01",
+    author: "Frank Herbert",
+    genres: ["science fiction", "novel", "adventure"],
+    hasMovieAdaptation: true,
+    pages: 658,
+    translations: {
+      spanish: "",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.25,
+        ratingsCount: 1142893,
+        reviewsCount: 49701,
+      },
+    },
+  },
+  {
+    id: 4,
+    title: "Harry Potter and the Philosopher's Stone",
+    publicationDate: "1997-06-26",
+    author: "J. K. Rowling",
+    genres: ["fantasy", "adventure"],
+    hasMovieAdaptation: true,
+    pages: 223,
+    translations: {
+      spanish: "Harry Potter y la piedra filosofal",
+      korean: "해리 포터와 마법사의 돌",
+      bengali: "হ্যারি পটার এন্ড দ্য ফিলোসফার্স স্টোন",
+      portuguese: "Harry Potter e a Pedra Filosofal",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.47,
+        ratingsCount: 8910059,
+        reviewsCount: 140625,
+      },
+      librarything: {
+        rating: 4.29,
+        ratingsCount: 120941,
+        reviewsCount: 1960,
+      },
+    },
+  },
+  {
+    id: 5,
+    title: "A Game of Thrones",
+    publicationDate: "1996-08-01",
+    author: "George R. R. Martin",
+    genres: ["fantasy", "high-fantasy", "novel", "fantasy fiction"],
+    hasMovieAdaptation: true,
+    pages: 835,
+    translations: {
+      korean: "왕좌의 게임",
+      polish: "Gra o tron",
+      portuguese: "A Guerra dos Tronos",
+      spanish: "Juego de tronos",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.44,
+        ratingsCount: 2295233,
+        reviewsCount: 59058,
+      },
+      librarything: {
+        rating: 4.36,
+        ratingsCount: 38358,
+        reviewsCount: 1095,
+      },
+    },
+  },
+];
+
+function getBooks() {
+  return data;
+}
+
+function getBook(id) {
+  return data.find((d) => d.id === id);
+}
+
+// DESTRUCTURING
+
+const book = getBook(1);
+
+// const title = book.title;
+// const author = book.author;
+// title;
+// author;
+
+const { title, author, pages, genres, publicationDate, hasMovieAdaptation } =
+  book;
+
+// const primaryGenre = genres[0];
+// const secondaryGenre = genres[1];
+
+const [primaryGenre, secondaryGenre, ...otherGenres] = genres;
+
+const newGenres = [...genres, "epic fantasy"];
+console.log(newGenres);
+
+const updatedBook = {
+  ...book,
+  // ADDING A NEW PROPERTY
+  moviePublicationDate: "2001-12-19",
+  // OVERWRITING AN EXISTING PROPERTY
+  pages: 1210,
+};
+
+updatedBook;
+
+// TEMPLATE LITERALS
+
+const summary = `${title}, a ${pages}-pages long book, was writen by ${author} and published in ${
+  publicationDate.split("-")[0]
+}. The book has ${hasMovieAdaptation ? "" : "not"} been adapted as a movie.`;
+summary;
+
+// TERNARIES INSTEAD OF IF/ELSE STATEMENT
+
+const pagesRange = pages > 1000 ? "over a thousand" : "less than one thousand";
+pagesRange;
+
+// ARROW FUNCTION
+
+// function getDate(str) {
+//   return str.split("-")[0];
+// }
+
+const getDate = (str) => str.split("-")[0];
+console.log(getDate(publicationDate));
+
+// Short Circuiting and Logical operators
+console.log(true && "hi");
+console.log(false && "hi");
+console.log(hasMovieAdaptation && "This book has movie");
+
+// falsy: 0, null, undefined,""
+console.log(0 && " hello ");
+
+console.log(true || "Hello");
+console.log(false || "Hello");
+console.log(book.translations.spanish);
+const spanishTranslation = book.translations.spanish || "Not translated";
+spanishTranslation;
+
+console.log(book.reviews.librarything.reviewsCount);
+const countWrong = book.reviews.librarything.reviewsCount || "no data";
+countWrong;
+
+// const count = book.reviews.librarything.reviewsCount && "no data";
+
+// Nullish coalescing operator
+const count = book.reviews.librarything.reviewsCount ?? "no data";
+count;
+
+// OPTIONAL CHAINING
+
+function getTotalReviewCount(book) {
+  const goodread = book.reviews?.goodreads?.reviewsCount;
+  const librarything = book.reviews?.librarything?.reviewsCount ?? 0;
+
+  return goodread + librarything;
+}
+getTotalReviewCount(book);
+console.log(getTotalReviewCount(book));
+
+// The arrar map Method
