24e0369c55de92f81fe4d6bd0c98bfdd67030c81
edyta-cymer <edytacymer@gmail.com>
2023-07-21 19:54:52 +0200

    55. Challenge #2 Profile Card
    Section 05.
    55. Challenge #2 Profile Card (v2)

--- 05-challange-profile-card/src/components/SkillList.js
+++ 05-challange-profile-card/src/components/SkillList.js
@@ -1,17 +1,62 @@
 import React from "react";
 
 const SkillList = () => {
+  const skills = [
+    {
+      skill: "HTML+CSS",
+      level: "advanced",
+      color: "#2662EA",
+    },
+    {
+      skill: "JavaScript",
+      level: "advanced",
+      color: "#EFD81D",
+    },
+    {
+      skill: "Web Design",
+      level: "advanced",
+      color: "#C3DCAF",
+    },
+    {
+      skill: "Git and GitHub",
+      level: "intermediate",
+      color: "#E84F33",
+    },
+    {
+      skill: "React",
+      level: "advanced",
+      color: "#60DAFB",
+    },
+    {
+      skill: "Svelte",
+      level: "beginner",
+      color: "#FF3B00",
+    },
+  ];
+
   return (
     <div>
       <ul className="skill-list">
-        <li className="skill purple-color">JavaScript 💪</li>
-        <li className="skill blue-color">React 💪 </li>
-        <li className="skill pink-color">Git and Github 💪</li>
-        <li className="skill green-color">Web design 💪</li>
-        <li className="skill darker-pink-color">TypeScript ⌨️ </li>
+        {skills.map((skill) => (
+          <Skill skill={skill.skill} color={skill.color} level={skill.level} />
+        ))}
       </ul>
     </div>
   );
 };
+
+function Skill({ skill, color, level }) {
+  return (
+    <div className="skill">
+      <li className="skill" style={{ backgroundColor: color }}>
+        <p>{skill}</p>
+
+        {level === "beginner" && "👶"}
+        {level === "intermediate" && "👍"}
+        {level === "advanced" && "💪"}
+      </li>
+    </div>
+  );
+}
 
 export default SkillList;
