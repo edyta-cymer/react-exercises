0df0ca46a38435f1585b98f664c0e0819f25041c
edyta-cymer <edytacymer@gmail.com>
2023-07-21 16:22:17 +0200

    52. React Fragments
    Section 05.
    52. React Fragments

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -60,17 +60,22 @@ function Menu() {
   const pizzasNum = pizzas.length;
   return (
     <main className="menu">
-      <h2>Our Menu</h2>
+      <h2>Our Menu</h2>;
       {pizzasNum > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone over, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu. Please come back later.</p>
       )}
-
       {/* <Pizza
         name="Pizza Spinaci"
         ingredient="Tomato, mozarella, spinach, and ricotta cheese"
