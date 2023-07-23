824a0481b2efbaef86ec336e6fb58a31d3754696
edyta-cymer <edytacymer@gmail.com>
2023-07-23 15:21:38 +0200

    65. Updating State Based on Current State
    Section 06.
    65. Updating State Based on Current State

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -12,17 +12,18 @@ export default function App() {
 
   function handlePrevious() {
     if (step > 1) {
-      setStep(step - 1);
+      setStep((s) => s - 1);
     }
   }
   function handleNext() {
     if (step < 3) {
-      setStep(step + 1);
+      setStep((s) => s + 1);
+      // setStep((s) => s + 1);
     }
   }
   return (
     <>
-      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+      <button className="close" onClick={() => setIsOpen((is) => !isOpen)}>
         &times;
       </button>
       {isOpen && (
