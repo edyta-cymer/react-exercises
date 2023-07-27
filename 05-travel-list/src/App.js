53cbe0ee12e44ad91896f829858166a7bcdfd50d
edyta-cymer <edytacymer@gmail.com>
2023-07-27 12:26:35 +0200

    71. Rendering the Items List
    Section 06.
    71. Rendering the Items List

---
+++ 05-travel-list/src/App.js
@@ -0,0 +1,42 @@
+import Logo from "./components/Logo";
+import Form from "./components/Form";
+// import PackingList from "./components/PackingList";
+import Stats from "./components/Stats";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: true },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
+export default function App() {
+  return (
+    <div>
+      <Logo />
+      <Form />
+      <Stats />
+      <PackingList />
+    </div>
+  );
+}
+const PackingList = () => {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+        <button>❌</button>
+      </span>
+    </li>
+  );
+}
---
+++ 05-travel-list/src/components/Logo.js
@@ -0,0 +1,4 @@
+function Logo() {
+  return <h1>🏝 Far Away 🧳</h1>;
+}
+export default Logo;
