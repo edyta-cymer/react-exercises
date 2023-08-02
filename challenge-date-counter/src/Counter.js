7eb0d8777f27c11c738c3c0f1531f61ac450ce6b
edyta-cymer <edytacymer@gmail.com>
2023-08-02 15:37:46 +0200

    76. Challenge #2 Date Counter (v2)
    Section 06.
    76. Challenge #2 Date Counter (v2)

--- challenge-date-counter/src/Counter.js
+++ challenge-date-counter/src/Counter.js
@@ -7,22 +7,35 @@ function Counter() {
   const date = new Date("june 27 2023");
   date.setDate(date.getDate() + count);
 
+  function handleReset() {
+    setStep(1);
+    setCount(0);
+    // console.log("Tak udalo sie zresetowac");
+  }
+
   return (
     <div className="container">
       <div>
-        <button className="btn" onClick={() => setStep((c) => c - 1)}>
-          -
-        </button>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        ></input>
+
         <span className="span">Steps: {step}</span>
-        <button className="btn" onClick={() => setStep((c) => c + 1)}>
-          +
-        </button>
       </div>
       <div>
         <button className="btn" onClick={() => setCount((c) => c - step)}>
           -
         </button>
-        <span className="span">Count: {count}</span>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        ></input>
+
         <button className="btn" onClick={() => setCount((c) => c + step)}>
           +
         </button>
@@ -36,6 +49,14 @@ function Counter() {
             : `${Math.abs(count)} days ago was: `}
           {date.toDateString()}
         </h2>
+
+        {count !== 0 || step !== 1 ? (
+          <div>
+            <button className="btn" onClick={(e) => handleReset()}>
+              RESET
+            </button>
+          </div>
+        ) : null}
       </div>
     </div>
   );
