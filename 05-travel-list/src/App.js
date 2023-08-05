a6080a05513ba1118a3245bf94b57aaa49659cef
edyta-cymer <edytacymer@gmail.com>
2023-08-05 18:28:31 +0200

    86. Sorting Items
    Section 07.
    86. Sorting Items

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -36,10 +36,29 @@ export default function App() {
 }
 
 const PackingList = ({ items, onDeleteItem, onToggleItems }) => {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+  if (sortBy === "input") {
+    sortedItems = items;
+  }
+
+  if (sortBy === "description") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  }
+
+  if (sortBy === "packed") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  }
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -48,6 +67,13 @@ const PackingList = ({ items, onDeleteItem, onToggleItems }) => {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 };
