1d666320d57883173f13a82492e17f4e3de4f4ed
edyta-cymer <edytacymer@gmail.com>
2023-08-24 12:10:44 +0200

    115. Building a Reusable Star Rating Component.
    Section 10.
    115. Building a Reusable Star Rating Component

---
+++ 07-usePopcorn/src/StarRating.js
@@ -0,0 +1,25 @@
+export default function StarRating({ maxRating = 5 }) {
+  const containerStyle = {
+    display: "flex",
+    alignItems: "center",
+    gap: "16px",
+  };
+  const starContainerStyle = {
+    display: "flex",
+    gap: "4px",
+  };
+  const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+  };
+  return (
+    <div style={containerStyle}>
+      <div style={starContainerStyle}>
+        {Array.from({ length: maxRating }, (_, i) => (
+          <span>S{i + 1}</span>
+        ))}
+      </div>
+      <p style={textStyle}>10</p>
+    </div>
+  );
+}
