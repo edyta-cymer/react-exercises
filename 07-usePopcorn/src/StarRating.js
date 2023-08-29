3cfa36ff1b5ea19cff503294a15e769a165c5812
edyta-cymer <edytacymer@gmail.com>
2023-08-29 15:15:13 +0200

    117. Handling Hover Effects
    Section 10.
    117. Handling Hover Effects

--- 07-usePopcorn/src/StarRating.js
+++ 07-usePopcorn/src/StarRating.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 export default function StarRating({ maxRating = 5 }) {
   const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState();
 
   function handleRating(rating) {
     setRating(rating);
@@ -14,11 +15,13 @@ export default function StarRating({ maxRating = 5 }) {
           <Star
             key={i}
             onRate={() => handleRating(i + 1)}
-            full={rating >= i + 1}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
           />
         ))}
       </div>
-      <p style={textStyle}>{rating || ""}</p>
+      <p style={textStyle}>{tempRating || rating || ""}</p>
     </div>
   );
 }
@@ -40,9 +43,15 @@ const starStyle = {
   display: "block",
   cursor: "pointer",
 };
-function Star({ onRate, full }) {
+function Star({ onRate, full, onHoverIn, onHoverOut }) {
   return (
-    <span role="button" style={starStyle} onClick={onRate}>
+    <span
+      role="button"
+      style={starStyle}
+      onClick={onRate}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+    >
       {full ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
